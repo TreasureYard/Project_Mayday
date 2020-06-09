27d3f7f2c430a87c7c20fe256b59d20f84a85445
YapYongLiang <yongliangyap2000@gmail.com>
2020-06-09 19:19:44 +0800

    solider follow movement version 1
    solider cannot turn behind yet

---
+++ Assets/CodeFiles/EnemyTropper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTropper : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Arm;
+    private SpriteRenderer SR;
+    private SpriteRenderer SRarm;
+    public PlayerDetect PD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SRarm = Arm.GetComponent<SpriteRenderer>();
+        SR = GetComponent<SpriteRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Angle in Radians
+        float AngleRad = Mathf.Atan2(Arm.transform.position.y- Player.transform.position.y, Arm.transform.position.x - Player.transform.position.x);
+
+        // Rotate Object
+        float AngleDeg = (180 / Mathf.PI) * AngleRad; //+ addangle;
+
+
+        if (AngleDeg >= -35 && PD.playerDetected && Vector3.Distance(Player.transform.position, transform.position) < 100) 
+        {
+            Arm.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+        }
+        
+       
+       
+    }
+
+    
+}
---
+++ Assets/CodeFiles/PlayerDetect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetect : MonoBehaviour
+{
+    public bool playerDetected;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerDetected = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerDetected = false;
+        }
+    }
+}
