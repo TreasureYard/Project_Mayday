4205d28058b50a2488fbd10e2086703e6f6fd6a4
YapYongLiang <yongliangyap2000@gmail.com>
2020-06-20 22:47:12 +0800

    looting and weight system
    right click to pick up, spacebar to drop latest loot, max of 4 loot

---
+++ Assets/CodeFiles/Objects/Loot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    public GameObject Player;
+    public LootManager LM;
+    private Rigidbody2D rb2d;
+    private bool followplayer;
+    public float objectspeed = 1000;
+    private void Start()
+    {
+        LM = Player.transform.GetChild(1).GetComponent<LootManager>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1) && Vector2.Distance(Player.transform.position, transform.position) < 15)
+        {
+            //transform.position = Player.transform.GetChild(1).transform.position;
+            rb2d.gravityScale = 0;
+            followplayer = true;
+            addLoot();
+
+            transform.parent = Player.transform.GetChild(1).transform; 
+
+            
+          
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(followplayer)
+        {
+            rb2d.AddForce((Player.transform.GetChild(1).transform.position - transform.position).normalized * objectspeed * Time.deltaTime, ForceMode2D.Force);
+            
+            Debug.Log("moving");
+        }
+    }
+
+    public void OutOfSpace()
+    {
+        rb2d.gravityScale = 1;
+        followplayer = false;
+        transform.parent = null;
+        LM.LootInventory.Remove(this.gameObject);
+        Debug.Log("Out of Space");
+    }
+
+    public void DropLoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.gravityScale = 1;
+            followplayer = false;
+            transform.parent = null;
+            LM.LootInventory.Remove(this.gameObject);
+            Debug.Log("Dropped");
+        }
+    }
+
+    void addLoot()
+    {
+
+        LM.LootInventory.Add(this.gameObject);
+    }
+}
---
+++ Assets/CodeFiles/Objects/LootManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    public List<GameObject> LootInventory;
+    private int maxWeight = 4;
+    private int childcount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LootInventory = new List<GameObject>(maxWeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        childcount = this.gameObject.transform.childCount;
+        if (childcount > maxWeight)
+        {
+            if(this.gameObject.transform.GetChild(childcount - 1).TryGetComponent<Loot>(out var loot))
+            {
+                loot.OutOfSpace();
+            }
+        }
+        else if(childcount != 0)
+        {
+            if (this.gameObject.transform.GetChild(childcount - 1).TryGetComponent<Loot>(out var loot))
+            {
+                loot.DropLoot();
+            }
+        }
+       
+        
+    }
+}
