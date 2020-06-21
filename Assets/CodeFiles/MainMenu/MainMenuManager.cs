5c40b250a756c1e2a3ac193dd42a2ed731851c49
YapYongLiang <yongliangyap2000@gmail.com>
2020-06-21 14:32:14 +0800

    new main menu bg

---
+++ Assets/CodeFiles/MainMenu/MainMenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject[] mainMenuobj;
+    public GameObject StartM;
+    private TextMeshProUGUI tmpstart;
+    private Color pingpongA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartM.SetActive(true);
+        tmpstart = StartM.GetComponent<TextMeshProUGUI>();
+        pingpongA = tmpstart.color;
+        foreach (GameObject m in mainMenuobj)
+        {
+            m.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(StartM.activeSelf)
+        {
+          
+            if (Input.anyKeyDown)
+            {
+                
+                ShowMainMenu();
+            }
+            else
+            {
+                pingpongA.a = Mathf.PingPong(Time.time, 1.8f);
+                tmpstart.color = pingpongA;
+
+            }
+        }
+        else
+        {
+            pingpongA.a = 1;
+            tmpstart.color = pingpongA;
+        }
+    }
+
+    void ShowMainMenu()
+    {
+        foreach (GameObject m in mainMenuobj)
+        {
+            m.SetActive(true);
+        }
+
+        StartM.SetActive(false);
+    }
+}
---
+++ Assets/CodeFiles/MainMenu/MoveTowardsPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPos : MonoBehaviour
+{
+    public GameObject btnPos;
+    private RectTransform btnrec;
+    private void Start()
+    {
+        btnrec = btnPos.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if(this.gameObject.activeSelf)
+        {
+            RectTransform myRectTransform = GetComponent<RectTransform>();
+
+            if (myRectTransform.anchoredPosition.x >= btnrec.anchoredPosition.x)
+            {
+                
+                myRectTransform.anchoredPosition += Vector2.left * 800 * Time.smoothDeltaTime;
+                Debug.Log("a");
+            } 
+           
+
+        }
+    }
+}
