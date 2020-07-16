8f60ecca4b7f3c03161aeeae93fa6a924c9c4a8d
YapYongLiang <yongliangyap2000@gmail.com>
2020-07-16 20:10:17 +0800

    shooting tutorial done

---
+++ Assets/CodeFiles/Objects/TargetNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetNode : MonoBehaviour
+{
+    SpriteRenderer SR;
+
+    private void Start()
+    {
+        SR = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "projectile")
+        {
+            SR.color = Color.green;
+        }
+        Destroy(collision.gameObject);
+    }
+
+
+}
---
+++ Assets/CodeFiles/Objects/TargetNodeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetNodeManager : MonoBehaviour
+{
+    public List<SpriteRenderer> SRnodes;
+    public GameObject Door;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < SRnodes.Count; i++)
+        {
+            if(SRnodes[i].color == Color.green)
+            {
+                SRnodes.RemoveAt(i);
+               
+            }
+
+            if (SRnodes.Count == 0)
+            {
+                Door.SetActive(false);
+            }
+        }
+    }
+}
