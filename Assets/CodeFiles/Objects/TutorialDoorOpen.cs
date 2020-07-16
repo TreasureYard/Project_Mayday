051d0980a979c459172848d481bea05e9737be1c
YapYongLiang <yongliangyap2000@gmail.com>
2020-07-16 12:05:52 +0800

    tutorial room backup 1

---
+++ Assets/CodeFiles/Objects/TutorialDoorOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDoorOpen : MonoBehaviour
+{
+    public List<GameObject> nodes;
+    public GameObject Door;
+    
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < nodes.Count; i++)
+        {
+            if(nodes[i].gameObject.GetComponent<SpriteRenderer>().color == Color.green)
+            {
+                nodes.RemoveAt(i);
+                if(nodes.Count == 0)
+                {
+                    Door.SetActive(false);
+                }
+            }
+            
+            
+        }
+    }
+}
